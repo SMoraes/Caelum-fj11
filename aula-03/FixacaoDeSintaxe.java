2ea72b61f382512876d4a63748e1fbd6670954b4
Sergio Adriano Rocha de Moraes <Sergio.tec1@hotmail.com>
2015-02-26 11:59:24 -0300

    Fixacão de Sintaxe

---
+++ aula-03/FixacaoDeSintaxe.java
@@ -0,0 +1,23 @@
+class FixacaoDeSintaxe {
+	public static void main(String[] args) {
+	//Imprima todos os números de 150 a 300	
+	/*	for(int i = 150; i<=300; i++){
+			System.out.println(i);
+		}
+	*/
+		/*
+		//Imprima a soma de 1 até 1000
+		for(int i = 1; i <= 1000; i++){
+			System.out.println(i);
+		}
+		*/
+			
+		//Imprima todos os multiplos de 3,entre 1 e 100
+		/*for (int i = 1; i <= 100 ; i++){
+			if (i % 3 == 0) {
+				System.out.println(i + " é múltiplo de 3.");
+			}
+		}
+		*/
+	}
+}
